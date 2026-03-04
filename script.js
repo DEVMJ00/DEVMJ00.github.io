d3f98215ea86493719782b4b6f1cfdc4c0a0e67a
Mario <152850961+DEVMJ00@users.noreply.github.com>
2026-03-04 22:36:40 +0100

    Refactor scrollToSection function and event listeners

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
 const burger = document.getElementById('burger');
 const navLinks = document.getElementById('navLinks');
 const links = document.querySelectorAll('.nav-links a');
 
 burger.addEventListener('click', () => {
-navLinks.classList.toggle('active');
+    navLinks.classList.toggle('active');
 });
 
 links.forEach(link => {
-link.addEventListener('click', () => {
-navLinks.classList.remove('active');
-});
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
 });
